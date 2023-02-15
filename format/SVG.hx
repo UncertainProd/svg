c229f23d5bcd80df2a3e42bb738d968c2cd064f4
UncertainProd <83609901+UncertainProd@users.noreply.github.com>
2023-02-15 12:30:52 +0530

    Backwards compat with openfl/svg is back!

--- format/SVG.hx
+++ format/SVG.hx
@@ -18,10 +18,24 @@ class SVG
 		}
 	}
 
-	public function render(graphics:Graphics, matrix:Matrix, ?inLayer:String = null)
+	public function render(graphics:Graphics, ?x:Float = 0, ?y:Float = 0, ?width:Int = -1, ?height:Int = -1, ?matrix:Matrix, ?inLayer:String = null)
 	{
 		if (data == null)
 			return;
+
+		// backwards compatibility with openfl/svg
+		var finalMatrix = new Matrix();
+		finalMatrix.identity();
+
+		if (width > -1 && height > -1)
+		{
+			finalMatrix.scale(width / data.width, height / data.height);
+		}
+
+		finalMatrix.translate(x, y);
+
+		if (matrix != null)
+			finalMatrix.concat(matrix);
 
 		var renderer = new SVGRenderer(data, inLayer);
 
